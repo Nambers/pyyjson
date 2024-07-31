5722891c1fe877e5f514948e0af7c2be63d6ecee
antares <antares@alyr.dev>
2024-08-01 00:58:45 +0800

    Add dev tools

---
+++ dev_tools/py_requirements.nix
@@ -0,0 +1,15 @@
+pypkgs: with pypkgs;
+[
+  # add packages here
+  orjson
+  pip
+  pytest
+  # needed by tests
+  arrow
+  faker
+  psutil
+  pytest-random-order
+  pytest-xdist
+  pytz
+  objgraph
+]
---
+++ dev_tools/shell.nix
@@ -0,0 +1,84 @@
+let
+  pkgs = import <nixpkgs> { };
+  # define version
+  using_python = (pkgs.enableDebugging pkgs.python312);
+  # import required python packages
+  required_python_packages = import ./py_requirements.nix;
+  #
+  nix_pyenv_directory = ".nix-pyenv";
+  pyenv = using_python.withPackages required_python_packages;
+in
+pkgs.mkShell {
+  packages = [
+    (pkgs.enableDebugging using_python)
+    pkgs.cmake
+    pkgs.gdb
+    pkgs.valgrind
+    # pkgs.gcc
+    pkgs.clang
+    pkgs.gcc
+  ];
+  shellHook = ''
+    cd ${builtins.toString ./.}
+
+    # ensure the nix-pyenv directory exists
+    if [[ ! -d ${nix_pyenv_directory} ]]; then mkdir ${nix_pyenv_directory}; fi
+    if [[ ! -d ${nix_pyenv_directory}/lib ]]; then mkdir ${nix_pyenv_directory}/lib; fi
+    if [[ ! -d ${nix_pyenv_directory}/bin ]]; then mkdir ${nix_pyenv_directory}/bin; fi
+
+    ensure_symlink() {
+        local link_path="$1"
+        local target_path="$2"
+        if [[ -L "$link_path" ]] && [[ "$(readlink "$link_path")" = "$target_path" ]]; then
+            return 0
+        fi
+        rm -f "$link_path" > /dev/null 2>&1
+        ln -s "$target_path" "$link_path"
+    }
+
+    # creating python library symlinks
+    for file in ${pyenv}/${using_python.sitePackages}/*; do
+        basefile=$(basename $file)
+        if [ -d "$file" ]; then
+            if [[ "$basefile" != *dist-info && "$basefile" != __pycache__ ]]; then
+                ensure_symlink ${nix_pyenv_directory}/lib/$basefile $file
+            fi
+        else
+            # the typing_extensions.py will make the vscode type checker not working!
+            if [[ $basefile == *.so ]] || ([[ $basefile == *.py ]] && [[ $basefile != typing_extensions.py ]]); then
+                ensure_symlink ${nix_pyenv_directory}/lib/$basefile $file
+            fi
+        fi
+    done
+    for file in ${nix_pyenv_directory}/lib/*; do
+        if [[ -L "$file" ]] && [[ "$(dirname $(readlink "$file"))" != "${pyenv}/${using_python.sitePackages}" ]]; then
+            rm -f "$file"
+        fi
+    done
+
+    # ensure the typing_extensions.py is not in the lib directory
+    rm ${nix_pyenv_directory}/lib/typing_extensions.py > /dev/null 2>&1
+
+    # add python executable to the bin directory
+    ensure_symlink ${nix_pyenv_directory}/bin/python ${using_python}/bin/python
+    export PATH=${using_python}/bin:${nix_pyenv_directory}/bin:$PATH
+
+    nix-build shell.nix -A inputDerivation -o ${nix_pyenv_directory}/.nix-shell-inputs
+
+    # custom
+    ensure_symlink ${nix_pyenv_directory}/bin/valgrind ${pkgs.valgrind}/bin/valgrind
+    export CC=${pkgs.clang}/bin/clang
+    export CXX=${pkgs.clang}/bin/clang++
+    ensure_symlink ${nix_pyenv_directory}/bin/clang $CC
+    ensure_symlink ${nix_pyenv_directory}/bin/clang++ $CXX
+    ensure_symlink ${nix_pyenv_directory}/bin/cmake ${pkgs.cmake}/bin/cmake
+    echo ${using_python.src}
+    # unzip the source
+    mkdir -p build
+    cd build
+    tar xvf ${using_python.src} > /dev/null
+    cd ..
+    # python lib
+    export PYTHONPATH=${pyenv}/${using_python.sitePackages}:$PYTHONPATH
+  '';
+}
