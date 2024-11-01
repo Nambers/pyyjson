c3a242d71e99b9798d2f801d63b4484c87dd8715
antares <antares@alyr.dev>
2024-11-02 00:21:13 +0800

    Update build.sh

--- dev_tools/build.sh
+++ dev_tools/build.sh
@@ -1,17 +1,41 @@
-mkdir -p build
-x=$(echo "$(python --version)" | cut -d'.' -f2)
+#!/bin/bash
+
+# TARGET_BUILD_TYPE=Debug
+# TARGET_BUILD_TYPE=Release
+TARGET_BUILD_TYPE=RelWithDebInfo
+
+CUR_PYVER=$(echo "$(python --version)" | cut -d'.' -f2)
 y=$(cat build/pyver 2>/dev/null)
-if [[ $x != $y ]]; then
-    echo "py ver mismatch"
+if [[ $CUR_PYVER != $y ]]; then
+    echo "py ver mismatch, removing build dir"
     rm -rf build
-    mkdir build
 fi
-echo $x > build/pyver
+
+# 定义一个函数来提取CMAKE_BUILD_TYPE的值
+get_cmake_build_type() {
+    if [ -f "./build/CMakeCache.txt" ]; then
+        build_type=$(grep "CMAKE_BUILD_TYPE" ./build/CMakeCache.txt | cut -d'=' -f2)
+    else
+        build_type=""
+    fi
+
+    if [[ $build_type != "" && $TARGET_BUILD_TYPE != $build_type ]]; then
+        echo "CMAKE_BUILD_TYPE mismatch, removing build dir"
+        rm -rf build
+    fi
+
+    # 返回结果
+    echo "提取到的值是: '$build_type'"
+}
+
+# 调用函数
+get_cmake_build_type
+
+mkdir -p build
+echo $CUR_PYVER > build/pyver
 cd build
 
 set -e
 
-# cmake .. -DCMAKE_BUILD_TYPE=Debug
-# cmake --build . --config Debug
-cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build . --config Release
+cmake .. -DCMAKE_BUILD_TYPE=$TARGET_BUILD_TYPE
+cmake --build . --config $TARGET_BUILD_TYPE
