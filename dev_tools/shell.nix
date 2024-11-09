8a8bafdc2a250d15c4bcf430d55ba929c9f75726
antares <antares@alyr.dev>
2024-11-09 15:06:17 +0800

    Update shell.nix

--- dev_tools/shell.nix
+++ dev_tools/shell.nix
@@ -14,10 +14,17 @@ let
               { }
             else
               {
-                setuptools = super.setuptools.overridePythonAttrs (superAttr: {
-                  patches = superAttr.patches ++ [
-                    ./fix.patch
-                  ];
+                setuptools = super.setuptools.overridePythonAttrs (superAttr: rec {
+                  version = "75.1.1";
+                  src = pkgs.fetchFromGitHub {
+                    owner = "pypa";
+                    repo = "setuptools";
+                    rev = "refs/tags/v${version}";
+                    hash = "sha256-b8O/DrDWAbD6ht9M762fFN6kPtV8hAbn1gAN9SS7H5g=";
+                  };
+                  # patches = superAttr.patches ++ [
+                  #   ./fix.patch
+                  # ];
                 });
               }
           );
