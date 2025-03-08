2f714888caf119a6b6de3d199bf5a295fd6a98c4
antares <antares@alyr.dev>
2025-03-09 02:35:01 +0800

    Fix inspect (found by python test)

--- src/pyyjson.c
+++ src/pyyjson.c
@@ -30,7 +30,7 @@ PyObject *pyyjson_DecodeFile(PyObject *self, PyObject *args, PyObject *kwargs);
 PyObject *run_unicode_accumulate_benchmark(PyObject *self, PyObject *args, PyObject *kwargs);
 PyObject *run_object_accumulate_benchmark(PyObject *self, PyObject *args, PyObject *kwargs);
 PyObject *run_object_benchmark(PyObject *self, PyObject *args, PyObject *kwargs);
-PyObject *inspect_pyunicode(PyObject *self, PyObject *args, PyObject*kwargs);
+PyObject *inspect_pyunicode(PyObject *self, PyObject *args, PyObject *kwargs);
 #endif
 PyObject *pyyjson_print_current_features(PyObject *self, PyObject *);
 
@@ -38,10 +38,10 @@ PyObject *JSONDecodeError = NULL;
 PyObject *JSONEncodeError = NULL;
 
 static PyMethodDef pyyjson_Methods[] = {
-        {"encode", (PyCFunction)pyyjson_Encode, METH_VARARGS | METH_KEYWORDS, "Converts arbitrary object recursively into JSON."},
-        {"decode", (PyCFunction)pyyjson_Decode, METH_VARARGS | METH_KEYWORDS, "Converts JSON as string to dict object structure."},
-        {"dumps", (PyCFunction)pyyjson_Encode, METH_VARARGS | METH_KEYWORDS, "Converts arbitrary object recursively into JSON."},
-        {"loads", (PyCFunction)pyyjson_Decode, METH_VARARGS | METH_KEYWORDS, "Converts JSON as string to dict object structure."},
+        {"encode", (PyCFunction)pyyjson_Encode, METH_VARARGS | METH_KEYWORDS, "dumps(obj, indent=None)\n--\n\nConverts arbitrary object recursively into JSON."},
+        {"decode", (PyCFunction)pyyjson_Decode, METH_VARARGS | METH_KEYWORDS, "decode(s)\n--\n\nConverts JSON as string to dict object structure."},
+        {"dumps", (PyCFunction)pyyjson_Encode, METH_VARARGS | METH_KEYWORDS, "dumps(obj, indent=None)\n--\n\nConverts arbitrary object recursively into JSON."},
+        {"loads", (PyCFunction)pyyjson_Decode, METH_VARARGS | METH_KEYWORDS, "loads(s)\n--\n\nConverts JSON as string to dict object structure."},
 #if PYYJSON_BUILD_BENCHMARK
         {"run_unicode_accumulate_benchmark", (PyCFunction)run_unicode_accumulate_benchmark, METH_VARARGS | METH_KEYWORDS, "Benchmark."},
         {"run_object_accumulate_benchmark", (PyCFunction)run_object_accumulate_benchmark, METH_VARARGS | METH_KEYWORDS, "Benchmark."},
