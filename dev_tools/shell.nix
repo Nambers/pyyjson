f10b83fb8a275f68f5e578d68ef6deef74231f51
antares <antares@alyr.dev>
2025-04-13 17:40:48 +0800

    Update nix files

--- dev_tools/shell.nix
+++ dev_tools/shell.nix
@@ -9,14 +9,15 @@ let
   pythonVerConfig = pkgs.lib.importJSON ./pyver.json;
   curVer = pythonVerConfig.curVer;
   leastVer = pythonVerConfig.minSupportVer;
-  drvs = pkgs.callPackage ./_drvs.nix {inherit pkgs-24-05; };
+  drvs = pkgs.callPackage ./_drvs.nix { inherit pkgs-24-05; };
   using_pythons = drvs.using_pythons;
   using_python = builtins.elemAt using_pythons (curVer - leastVer);
   pyenvs = drvs.pyenvs;
   pyenv = builtins.elemAt pyenvs (curVer - leastVer);
 in
 (pkgs.mkShell {
-  packages = pkgs.callPackage ./packages.nix { inherit pkgs-24-05;};
+  buildInputs = [ drvs.llvmDbg ];
+  packages = pkgs.callPackage ./packages.nix { inherit pkgs-24-05; };
   hardeningDisable = [ "fortify" ];
 
 })
