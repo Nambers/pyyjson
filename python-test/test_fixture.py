f273e47a6862df637eae74e7a3c2b38bc91ed23a
antares <antares@alyr.dev>
2025-06-02 22:56:22 +0800

    Update test

--- python-test/test_fixture.py
+++ python-test/test_fixture.py
@@ -8,14 +8,14 @@ from util import read_fixture_bytes, read_fixture_str
 
 
 class TestFixture:
-    def test_twitter(self):
+    def test_apache(self):
         """
-        loads(),dumps() twitter.json
+        loads(), dumps() apache.json
         """
-        val = read_fixture_str("twitter.json")
+        val = read_fixture_str("apache.json")
         read = pyyjson.loads(val)
         assert pyyjson.loads(pyyjson.dumps(read)) == read
-        assert pyyjson.dumps(read).encode("utf-8") == pyyjson.dumps_to_bytes(read)
+        assert pyyjson.loads(pyyjson.dumps_to_bytes(read)) == read
 
     def test_canada(self):
         """
@@ -28,7 +28,7 @@ class TestFixture:
 
     def test_citm_catalog(self):
         """
-        loads(), dumps() citm_catalog.json
+        loads(), dumps() ctm.json
         """
         val = read_fixture_str("ctm.json")
         read = pyyjson.loads(val)
@@ -43,6 +43,60 @@ class TestFixture:
         read = pyyjson.loads(val)
         assert pyyjson.loads(pyyjson.dumps(read)) == read
         assert pyyjson.loads(pyyjson.dumps_to_bytes(read)) == read
+
+    def test_instruments(self):
+        """
+        loads(), dumps() instruments.json
+        """
+        val = read_fixture_str("instruments.json")
+        read = pyyjson.loads(val)
+        assert pyyjson.loads(pyyjson.dumps(read)) == read
+        assert pyyjson.loads(pyyjson.dumps_to_bytes(read)) == read
+
+    def test_mesh(self):
+        """
+        loads(), dumps() mesh.json
+        """
+        val = read_fixture_str("mesh.json")
+        read = pyyjson.loads(val)
+        assert pyyjson.loads(pyyjson.dumps(read)) == read
+        assert pyyjson.loads(pyyjson.dumps_to_bytes(read)) == read
+
+    def test_mqaq2016(self):
+        """
+        loads(), dumps() MotionsQuestionsAnswersQuestions2016.json
+        """
+        val = read_fixture_str("MotionsQuestionsAnswersQuestions2016.json")
+        read = pyyjson.loads(val)
+        assert pyyjson.loads(pyyjson.dumps(read)) == read
+        assert pyyjson.loads(pyyjson.dumps_to_bytes(read)) == read
+
+    def test_truenull(self):
+        """
+        loads(), dumps() truenull.json
+        """
+        val = read_fixture_str("truenull.json")
+        read = pyyjson.loads(val)
+        assert pyyjson.loads(pyyjson.dumps(read)) == read
+        assert pyyjson.loads(pyyjson.dumps_to_bytes(read)) == read
+
+    def test_tweet(self):
+        """
+        loads(), dumps() tweet.json
+        """
+        val = read_fixture_str("tweet.json")
+        read = pyyjson.loads(val)
+        assert pyyjson.loads(pyyjson.dumps(read)) == read
+        assert pyyjson.loads(pyyjson.dumps_to_bytes(read)) == read
+
+    def test_twitter(self):
+        """
+        loads(),dumps() twitter.json
+        """
+        val = read_fixture_str("twitter.json")
+        read = pyyjson.loads(val)
+        assert pyyjson.loads(pyyjson.dumps(read)) == read
+        assert pyyjson.dumps(read).encode("utf-8") == pyyjson.dumps_to_bytes(read)
 
     def test_blns(self):
         """
